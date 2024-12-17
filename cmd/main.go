136a92154d8bd5f783d4685b4dde69d4ca832562
lemavisaitov <visaitovlema@gmail.com>
2024-12-17 18:43:24 +0300

    добавлена точка входа в программу

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/lemavisaitov/applied-informatics_3/internal/controller"
 
+func main() {
+	appController := controller.New()
+	appController.Run()
 }
